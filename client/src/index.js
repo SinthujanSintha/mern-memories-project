3e43875ba8428fe76652ca0775b871adde9b4e04
Sinthujan Punitharasa <sinthujan.p@eyepax.com>
2021-11-24 22:53:28 +0530

    Update simple comment

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material";
-//add command here
+//add coments here
 const theme = createTheme();
 ReactDOM.render(
   <Provider store={store}>
